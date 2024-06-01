539eebd91f9907a246027cc17597f6d6b75e93ef
Shubham Sharma <109148746+shubham001official@users.noreply.github.com>
2024-06-01 13:23:21 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,42 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Slider from "./_components/Slider";
+import GlobalApi from "./_utils/GlobalApi";
+import CategoryList from "./_components/CategoryList";
+import ProductList from "./_components/ProductList";
+import Footer from "./_components/Footer";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { Phone, Terminal } from "lucide-react";
+
+export default async function Home() {
+
+  
+  return (
+   <div className="p-10 px-5 md:px-16">
+  
+
+     {/* Sliders  */}
+     <Slider sliderList={sliderList} />
+     {/* Category List  */}
+     <CategoryList categoryList={categoryList}/>
+     {/* <Alert className="mt-10 mb-10" variant="success">
+      <Phone className="h-4 w-4" />
+      <AlertTitle>Contact Us</AlertTitle>
+      <AlertDescription>
+        Call us on +1 1234567890 to place and order and get deliver in 24 Hrs
+      </AlertDescription>
+    </Alert> */}
+    {/* Product List */}
+    <ProductList productList={productList} />
+
+    {/* Banner  */}
+    <Image src='/banner.png' width={1000} height={300}
+    alt="banner"
+    className="w-full h-[400px] object-contain"
+    />
+
+    {/* Footer  */}
+    <Footer/>
+   </div>
+  );
+}
